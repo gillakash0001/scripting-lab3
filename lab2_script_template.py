ef8c8fbd6fdfc9a53b25eef58dc911a5ccc8cf07
gillakash0001 <Asingh376@flemingcollege.ca>
2025-01-28 11:40:52 -0500

    Create lab2_script_template.py

---
+++ lab2_script_template.py
@@ -0,0 +1,55 @@
+def main():
+
+    # TODO: Step 2 - Create a complex data structure
+     team ={
+        'full_name':'Akashdeep singh' ,
+        'student_id':'10361224' ,
+        'pizza_toppings':[
+'ONION'
+'MUSHROOM'
+            'PINEAPPLE'
+        ] ,
+        movies:[
+            'title':'wednesday'
+            'genre':'horror'
+        ],
+        [ 'title':'stranger things'
+            'genre':'sci-fict'
+        ]
+        }
+
+       print(team) # Temporory Debug print
+
+    # TODO: Step 3 - Add another movie to the data structure
+    new_movie = {
+        'title':'marcko'
+        'genre':'action'
+    }
+    team['movies'].append(new _movie)
+    print(team) # Temporory Debug print
+# TODO: Step 4 - Function that prints student name and ID	
+def print_student_name_and_id(team):
+    id=team['10361224']
+    student_name=team['Akashdeep singh'].split()[1]
+
+    
+    return
+    
+# TODO: Step 5 - Function that adds pizza toppings to data structure
+def add_pizza_toppings(about_me, toppings):
+    return
+
+# TODO: Step 6 - Function that prints bullet list of pizza toppings
+def print_pizza_toppings(about_me):
+    return
+
+# TODO: Step 7 - Function that prints comma-separated list of movie genres
+def print_movie_genres(about_me):
+    return 
+
+# TODO: Step 8 - Function that prints comma-separated list of movie titles
+def print_movie_titles(movie_list):
+    return
+    
+if __name__ == '__main__':
+    main()
